fb832e5d28817ced87220834f73c5106ee3dd2b2
Samantha Peloquin <speloqu24@Gmail.com>
2021-11-08 13:47:41 -0700

    Update schema.sql
    #1w8ajvc

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS burgers_db;
 USE burgers_db;
 
--- If the table already exists, remove it before trying to create the table again
+-- If table exists, remove it before trying to create the table
 DROP TABLE IF EXISTS burgers;
 
 -- Create the burgers table
